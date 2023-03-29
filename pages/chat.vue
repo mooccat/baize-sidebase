533f052dba2d39e968a8a542d255f6a792de7fe9
lou <825681860@qq.com>
2023-03-29 17:59:08 +0800

    ✨ feat: add some

---
+++ pages/chat.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const { data: chat } = useFetch('/api/chat/chat', { method: 'POST', body: JSON.stringify({ text: 'client' }) })
+</script>
+
+<template>
+  <div>
+    Prisma ORM Data from the database, received {{ chat?.length || 0 }} records: <pre>{{ chat }}</pre>
+  </div>
+</template>
